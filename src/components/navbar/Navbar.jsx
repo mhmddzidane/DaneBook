d29d990bca706175892bbca5d00d868dead1d9e9
muhammadzidane633@gmail.com <muhammadzidane633@gmail.com>
2022-07-14 22:56:41 +0700

    First push

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">DaneBooking</span>
+        </Link>
+        {user ? (
+          user.username
+        ) : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <Link to="/login">
+              <button className="navButton">Login</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
